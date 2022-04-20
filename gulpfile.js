b9c1d4a2dbcdeb68ff349f18c9ebff40a0d9422b
KauztoKuro <ikkikurogane70@gmail.com>
2022-04-20 14:49:23 +0630

    L-21: Customizing the Library

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("scss/**/*.scss")
-    .pipe(sass())
-    .pipe(purgecss({ content: ['*.html'] }))
+  return src("sass/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(purgecss({ content: ["*.html"] }))
     .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["scss/**/*.scss", '*html'], buildStyles);
+  watch(["sass/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
